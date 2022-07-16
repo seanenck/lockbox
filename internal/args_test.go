5d49cf3a94ecdc58c6bdc69ee6bfc4bb0ebbd429
Sean Enck <enckse@voidedtech.com>
2022-07-16 09:19:01 -0400

    more tests for args

---
+++ internal/args_test.go
@@ -0,0 +1,50 @@
+package internal
+
+import (
+	"testing"
+)
+
+func TestClipArg(t *testing.T) {
+	for _, check := range []string{"-c", "-clip"} {
+		options := ParseArgs(check)
+		if !options.Clip {
+			t.Error("clip should be set")
+		}
+	}
+}
+
+func TestMultiArg(t *testing.T) {
+	for _, check := range []string{"-m", "-multi"} {
+		options := ParseArgs(check)
+		if !options.Multi {
+			t.Error("multi should be set")
+		}
+	}
+}
+
+func TestListArg(t *testing.T) {
+	for _, check := range []string{"-list", "-ls"} {
+		options := ParseArgs(check)
+		if !options.List {
+			t.Error("list should be set")
+		}
+	}
+}
+
+func TestOnce(t *testing.T) {
+	if options := ParseArgs("-once"); !options.Once {
+		t.Error("once should be set")
+	}
+}
+
+func TestShort(t *testing.T) {
+	if options := ParseArgs("-short"); !options.Short {
+		t.Error("short should be set")
+	}
+}
+
+func TestYes(t *testing.T) {
+	if options := ParseArgs("-yes"); !options.Yes {
+		t.Error("yes should be set")
+	}
+}
