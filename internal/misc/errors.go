c24711be0600a9262d1949eb7e25c6823b769a62
Sean Enck <enckse@voidedtech.com>
2022-09-13 14:04:10 -0400

    call is only used internally

--- internal/misc/errors.go
+++ internal/misc/errors.go
@@ -6,17 +6,12 @@ import (
 	"os"
 )
 
-// LogError will log an error to stderr.
-func LogError(message string, err error) {
+// Die will print messages and exit.
+func Die(message string, err error) {
 	msg := message
 	if err != nil {
 		msg = fmt.Sprintf("%s (%v)", msg, err)
 	}
 	fmt.Fprintln(os.Stderr, msg)
-}
-
-// Die will print messages and exit.
-func Die(message string, err error) {
-	LogError(message, err)
 	os.Exit(1)
 }
