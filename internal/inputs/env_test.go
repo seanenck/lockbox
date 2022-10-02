ace0e88ef575bcd395731776c5ed69ebd960c1c1
Sean Enck <enckse@voidedtech.com>
2022-10-02 16:20:18 -0400

    format totp token tests

--- internal/inputs/env_test.go
+++ internal/inputs/env_test.go
@@ -7,6 +7,27 @@ import (
 	"github.com/enckse/lockbox/internal/inputs"
 )
 
+func TestFormatTOTP(t *testing.T) {
+	otp := inputs.FormatTOTP("otpauth://abc")
+	if otp != "otpauth://abc" {
+		t.Errorf("invalid totp token: %s", otp)
+	}
+	otp = inputs.FormatTOTP("abc")
+	if otp != "otpauth://totp/lbissuer:lbaccount?algorithm=SHA1&digits=6&issuer=lbissuer&period=30&secret=abc" {
+		t.Errorf("invalid totp token: %s", otp)
+	}
+	os.Setenv("LOCKBOX_TOTP_FORMAT", "test/%s")
+	otp = inputs.FormatTOTP("abc")
+	if otp != "test/abc" {
+		t.Errorf("invalid totp token: %s", otp)
+	}
+	os.Setenv("LOCKBOX_TOTP_FORMAT", "")
+	otp = inputs.FormatTOTP("abc")
+	if otp != "otpauth://totp/lbissuer:lbaccount?algorithm=SHA1&digits=6&issuer=lbissuer&period=30&secret=abc" {
+		t.Errorf("invalid totp token: %s", otp)
+	}
+}
+
 func TestColorSetting(t *testing.T) {
 	os.Setenv("LOCKBOX_NOCOLOR", "yes")
 	c, err := inputs.IsNoColorEnabled()
