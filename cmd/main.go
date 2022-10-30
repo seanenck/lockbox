ec50ac69f2de3bd1fa6f38e0adf1019444a90db2
Sean Enck <enckse@voidedtech.com>
2022-10-30 09:57:49 -0400

    remove newline from version

--- cmd/main.go
+++ cmd/main.go
@@ -75,7 +75,7 @@ func processInfoCommands(command string, args []string) ([]string, error) {
 	case cli.HelpCommand:
 		return cli.Usage()
 	case cli.VersionCommand:
-		return []string{fmt.Sprintf("version: %s", strings.TrimSpace(version))}, nil
+		return []string{fmt.Sprintf("version: %s", version)}, nil
 	case cli.EnvCommand, cli.BashCommand:
 		defaultFlag := cli.BashDefaultsCommand
 		isEnv := command == cli.EnvCommand
