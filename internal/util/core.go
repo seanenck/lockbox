68142f6ecae65e05c2294134015064e1971c0f0a
Sean Enck <enckse@voidedtech.com>
2023-02-16 15:22:22 -0500

    adding this here

--- internal/util/core.go
+++ internal/util/core.go
@@ -73,3 +73,11 @@ func ReadStdin(one bool) ([]byte, error) {
 	}
 	return b.Bytes(), nil
 }
+
+// IfNotSet will return the value or the default if the value is the value's default setting
+func IfNotSet[T comparable](val, defaultTo T) T {
+	if val == *new(T) {
+		return defaultTo
+	}
+	return val
+}
