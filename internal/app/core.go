2db93c5436385349f96f9bf625fcc3094b2ff546
Sean Enck <enckse@voidedtech.com>
2023-07-25 15:17:38 -0400

    split core -> completion for cleanup

--- internal/app/core.go
+++ internal/app/core.go
@@ -2,7 +2,6 @@
 package app
 
 import (
-	"bytes"
 	"embed"
 	"fmt"
 	"io"
@@ -10,7 +9,6 @@ import (
 	"path/filepath"
 	"sort"
 	"strings"
-	"text/template"
 
 	"github.com/enckse/lockbox/internal/backend"
 	"github.com/enckse/lockbox/internal/inputs"
@@ -93,28 +91,6 @@ type (
 		args []string
 		tx   *backend.Transaction
 	}
-	// Completions handles the inputs to completions for templating
-	Completions struct {
-		Options             []string
-		CanClip             bool
-		CanTOTP             bool
-		ReadOnly            bool
-		InsertCommand       string
-		TOTPSubCommands     []string
-		TOTPListCommand     string
-		RemoveCommand       string
-		ClipCommand         string
-		ShowCommand         string
-		MultiLineCommand    string
-		MoveCommand         string
-		TOTPCommand         string
-		DoTOTPList          string
-		DoList              string
-		Executable          string
-		JSONCommand         string
-		HelpCommand         string
-		HelpAdvancedCommand string
-	}
 )
 
 // NewDefaultCommand creates a new app command
@@ -193,89 +169,6 @@ func exeName() (string, error) {
 		return "", err
 	}
 	return filepath.Base(n), nil
-}
-
-// GenerateCompletions handles creating shell completion outputs
-func GenerateCompletions(isBash, defaults bool) ([]string, error) {
-	name, err := exeName()
-	if err != nil {
-		return nil, err
-	}
-	c := Completions{
-		Executable:          name,
-		InsertCommand:       InsertCommand,
-		RemoveCommand:       RemoveCommand,
-		TOTPSubCommands:     []string{TOTPMinimalCommand, TOTPOnceCommand, TOTPShowCommand},
-		TOTPListCommand:     TOTPListCommand,
-		ClipCommand:         ClipCommand,
-		ShowCommand:         ShowCommand,
-		MultiLineCommand:    MultiLineCommand,
-		JSONCommand:         JSONCommand,
-		HelpCommand:         HelpCommand,
-		HelpAdvancedCommand: HelpAdvancedCommand,
-		TOTPCommand:         TOTPCommand,
-		MoveCommand:         MoveCommand,
-		DoList:              fmt.Sprintf("%s %s", name, ListCommand),
-		DoTOTPList:          fmt.Sprintf("%s %s %s", name, TOTPCommand, TOTPListCommand),
-		Options:             []string{MultiLineCommand, EnvCommand, HelpCommand, ListCommand, ShowCommand, VersionCommand, JSONCommand},
-	}
-	isReadOnly := false
-	isClip := true
-	isTOTP := true
-	if !defaults {
-		ro, err := inputs.IsReadOnly()
-		if err != nil {
-			return nil, err
-		}
-		isReadOnly = ro
-		noClip, err := inputs.IsNoClipEnabled()
-		if err != nil {
-			return nil, err
-		}
-		if noClip {
-			isClip = false
-		}
-		noTOTP, err := inputs.IsNoTOTP()
-		if err != nil {
-			return nil, err
-		}
-		if noTOTP {
-			isTOTP = false
-		}
-	}
-	c.CanClip = isClip
-	c.ReadOnly = isReadOnly
-	c.CanTOTP = isTOTP
-	if c.CanClip {
-		c.Options = append(c.Options, ClipCommand)
-		c.TOTPSubCommands = append(c.TOTPSubCommands, TOTPClipCommand)
-	}
-	if !c.ReadOnly {
-		c.Options = append(c.Options, MoveCommand, RemoveCommand, InsertCommand)
-		c.TOTPSubCommands = append(c.TOTPSubCommands, TOTPInsertCommand)
-	}
-	if c.CanTOTP {
-		c.Options = append(c.Options, TOTPCommand)
-	}
-	using, err := readDoc("zsh")
-	if err != nil {
-		return nil, err
-	}
-	if isBash {
-		using, err = readDoc("bash")
-		if err != nil {
-			return nil, err
-		}
-	}
-	t, err := template.New("t").Parse(using)
-	if err != nil {
-		return nil, err
-	}
-	var buf bytes.Buffer
-	if err := t.Execute(&buf, c); err != nil {
-		return nil, err
-	}
-	return []string{buf.String()}, nil
 }
 
 // Usage return usage information
--- internal/app/core_test.go
+++ internal/app/core_test.go
@@ -1,7 +1,6 @@
 package app_test
 
 import (
-	"os"
 	"strings"
 	"testing"
 
@@ -22,51 +21,6 @@ func TestUsage(t *testing.T) {
 			if len(l) > 79 {
 				t.Errorf("usage line > 79 (%d), line: %s", len(l), l)
 			}
-		}
-	}
-}
-
-func TestGenerateCompletions(t *testing.T) {
-	testCompletions(t, true)
-	testCompletions(t, false)
-}
-
-func testCompletions(t *testing.T, bash bool) {
-	os.Setenv("LOCKBOX_NOTOTP", "yes")
-	os.Setenv("LOCKBOX_READONLY", "yes")
-	os.Setenv("LOCKBOX_NOCLIP", "yes")
-	defaults, _ := app.GenerateCompletions(bash, true)
-	roNoTOTPClip, _ := app.GenerateCompletions(bash, false)
-	if roNoTOTPClip[0] == defaults[0] {
-		t.Error("should not match defaults")
-	}
-	os.Setenv("LOCKBOX_NOTOTP", "")
-	roNoClip, _ := app.GenerateCompletions(bash, false)
-	if roNoClip[0] == defaults[0] || roNoClip[0] == roNoTOTPClip[0] {
-		t.Error("should not equal defaults nor no totp/clip")
-	}
-	os.Setenv("LOCKBOX_READONLY", "")
-	os.Setenv("LOCKBOX_NOCLIP", "yes")
-	noClip, _ := app.GenerateCompletions(bash, false)
-	if roNoClip[0] == noClip[0] || noClip[0] == defaults[0] || noClip[0] == roNoTOTPClip[0] {
-		t.Error("readonly/noclip != noclip (nor defaults, nor ro/no totp/clip)")
-	}
-	os.Setenv("LOCKBOX_READONLY", "yes")
-	os.Setenv("LOCKBOX_NOCLIP", "")
-	ro, _ := app.GenerateCompletions(bash, false)
-	if roNoClip[0] == ro[0] || noClip[0] == ro[0] || ro[0] == defaults[0] || ro[0] == roNoTOTPClip[0] {
-		t.Error("readonly/noclip != ro (nor ro == noclip, nor ro == defaults)")
-	}
-	os.Setenv("LOCKBOX_READONLY", "")
-	os.Setenv("LOCKBOX_NOCLIP", "")
-	os.Setenv("LOCKBOX_NOTOTP", "")
-	isDefaultsToo, _ := app.GenerateCompletions(bash, false)
-	if isDefaultsToo[0] != defaults[0] {
-		t.Error("defaults should match env defaults")
-	}
-	for _, confirm := range [][]string{defaults, roNoClip, noClip, ro, isDefaultsToo} {
-		if len(confirm) != 1 {
-			t.Error("completions returned an invalid array")
 		}
 	}
 }
