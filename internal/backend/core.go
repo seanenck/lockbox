a1018a4a3927105d5d83e8d69085b548afcdf9a8
Sean Enck <enckse@voidedtech.com>
2023-07-26 17:46:07 -0400

    support keyfile only

--- internal/backend/core.go
+++ internal/backend/core.go
@@ -64,6 +64,9 @@ func getCredentials(key, keyFile string) (*gokeepasslib.DBCredentials, error) {
 		if !platform.PathExists(keyFile) {
 			return nil, errors.New("no keyfile found on disk")
 		}
+		if len(key) == 0 {
+			return gokeepasslib.NewKeyCredentials(keyFile)
+		}
 		return gokeepasslib.NewPasswordAndKeyCredentials(key, keyFile)
 	}
 	return gokeepasslib.NewPasswordCredentials(key), nil
