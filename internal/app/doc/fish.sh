4a0f4bfdca1e083725dbbf89e61fb7b7aacc9147
Sean Enck <enckse@voidedtech.com>
2024-03-22 18:55:36 -0400

    fix fish completion for help and totp subcommands

--- internal/app/doc/fish.sh
+++ internal/app/doc/fish.sh
@@ -5,7 +5,7 @@ complete -c {{ $.Executable }} -f
 function {{ $profile.Name }}
   set -l commands {{ range $idx, $value := $profile.Options }}{{ if gt $idx 0}} {{ end }}{{ $value }}{{ end }}
   complete -c {{ $.Executable }} -n "not __fish_seen_subcommand_from $commands" -a "$commands"
-  complete -c {{ $.Executable }} -n "__fish_seen_subcommand_from {{ $.HelpCommand }}" -a "{{ $.HelpAdvancedCommand }}"
+  complete -c {{ $.Executable }} -n "__fish_seen_subcommand_from {{ $.HelpCommand }}; and test (count (commandline -opc)) -lt 3" -a "{{ $.HelpAdvancedCommand }}"
 {{- if not $profile.ReadOnly }}
 {{- if $profile.CanList }}
   complete -c {{ $.Executable }} -n "__fish_seen_subcommand_from {{ $.InsertCommand }} {{ $.MultiLineCommand }} {{ $.RemoveCommand }}; and test (count (commandline -opc)) -lt 3" -a "({{ $.DoList }})"
@@ -14,7 +14,7 @@ function {{ $profile.Name }}
 {{- end}}
 {{- if $profile.CanTOTP }}
   set -l totps {{ $.TOTPListCommand }}{{ range $key, $value := .TOTPSubCommands }} {{ $value }}{{ end }}
-  complete -c {{ $.Executable }} -n "__fish_seen_subcommand_from {{ $.TOTPCommand }}" -a "$totps"
+  complete -c {{ $.Executable }} -n "__fish_seen_subcommand_from {{ $.TOTPCommand }}; and not __fish_seen_subcommand_from $totps" -a "$totps"
 {{- if $profile.CanList }}
   complete -c {{ $.Executable }} -n "__fish_seen_subcommand_from {{ $.TOTPCommand }}; and __fish_seen_subcommand_from $totps" -a "({{ $.DoTOTPList }})"
 {{- end}}
