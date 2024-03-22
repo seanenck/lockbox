4d9be3972aa6c316769577384fd88a62e9871e00
Sean Enck <enckse@voidedtech.com>
2024-03-21 21:52:47 -0400

    fish completions should work

---
+++ internal/app/doc/fish.sh
@@ -0,0 +1,44 @@
+complete -c {{ $.Executable }} -f
+
+{{- range $idx, $profile := $.Profiles }}
+
+function {{ $profile.Name }}
+  set -l commands {{ range $idx, $value := $profile.Options }}{{ if gt $idx 0}} {{ end }}{{ $value }}{{ end }}
+  complete -c {{ $.Executable }} -n "not __fish_seen_subcommand_from $commands" -a "$commands"
+  complete -c {{ $.Executable }} -n "__fish_seen_subcommand_from {{ $.HelpCommand }}" -a "{{ $.HelpAdvancedCommand }}"
+{{- if not $profile.ReadOnly }}
+{{- if $profile.CanList }}
+  complete -c {{ $.Executable }} -n "__fish_seen_subcommand_from {{ $.InsertCommand }} {{ $.MultiLineCommand }} {{ $.RemoveCommand }}; and test (count (commandline -opc)) -lt 3" -a "({{ $.DoList }})"
+  complete -c {{ $.Executable }} -n "__fish_seen_subcommand_from {{ $.MoveCommand }}; and test (count (commandline -opc)) -lt 4" -a "({{ $.DoList }})"
+{{- end}}
+{{- end}}
+{{- if $profile.CanTOTP }}
+  set -l totps {{ $.TOTPListCommand }}{{ range $key, $value := .TOTPSubCommands }} {{ $value }}{{ end }}
+  complete -c {{ $.Executable }} -n "__fish_seen_subcommand_from {{ $.TOTPCommand }}" -a "$totps"
+{{- if $profile.CanList }}
+  complete -c {{ $.Executable }} -n "__fish_seen_subcommand_from {{ $.TOTPCommand }}; and __fish_seen_subcommand_from $totps" -a "({{ $.DoTOTPList }})"
+{{- end}}
+{{- end}}
+{{- if $profile.CanList }}
+  complete -c {{ $.Executable }} -n "__fish_seen_subcommand_from {{ $.ShowCommand }} {{ $.JSONCommand }}{{ if $profile.CanClip }} {{ $.ClipCommand }} {{end}}; and test (count (commandline -opc)) -lt 3" -a "({{ $.DoList}})"
+{{- end}}
+end
+{{- end}}
+
+function {{ $.Executable }}-completions
+{{- if eq (len $.Profiles) 1 }}
+  {{ $.DefaultProfile.Name }}
+{{- else}}
+{{- range $idx, $profile := $.Profiles }}
+{{- if not $profile.IsDefault }}
+  if [ "{{ $.CompletionEnv }}" = "{{ $profile.Display }}" ]
+    {{ $profile.Name }}
+    return
+  end
+{{- end}}
+{{- end}}
+  {{ $.DefaultProfile.Name }} 
+{{- end}}
+end
+
+{{ $.Executable }}-completions
